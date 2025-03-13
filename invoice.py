3997acb2ef5c88238e381c1e5bb753e1fccb757c
harshith140131 <harshithdubasi14@gmail.com>
2025-03-13 23:04:52 +0530

    Initial commit

--- invoice.py
+++ invoice.py
@@ -23,7 +23,8 @@ def reset():
 
 # Function to load Google Gemini Pro Vision API And get response
 def get_gemini_repsonse(input, image, prompt):
-    model = genai.GenerativeModel('gemini-pro-vision')
+    model = genai.GenerativeModel('gemini-1.5-pro')
+
     response = model.generate_content([input, image[0], prompt])
     return response.text
 
